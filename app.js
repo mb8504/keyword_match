c9b2118c92a2f94e3901235b7ecbc7b114033406
mb8504 <mbergmarketing@gmail.com>
2023-08-04 13:34:48 -0400

    separated into strings

--- app.js
+++ app.js
@@ -2,10 +2,29 @@ const txt1 = document.getElementById('text_area1');
 const btn1 = document.getElementById('submitBtn1');
 const out1 = document.getElementById('text_area2');
 
+let terms = [];
+
 function matchType() {
-    out1.innerHTML = txt1.value;
+    let userKeywords = out1.innerHTML = txt1.value;
+    let arr = userKeywords.split('\n');
+
+    console.log(arr);
 }
 
 btn1.addEventListener('click',matchType);
+
+
+
+
+
+
+// let userKeywords = [];
+
+// function matchType() {
+//     userKeywords.push(out1.innerHTML = txt1.value);
+//     console.log(userKeywords);
+// }
+
+// btn1.addEventListener('click',matchType);
 
 
